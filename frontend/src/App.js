6169a3106215d23331aff8d5e27dd1d1a2a7826c
127127 <megan1220k@gmail.com>
2025-09-18 10:11:09 +0900

    첫 커밋

---
+++ frontend/src/App.js
@@ -0,0 +1,42 @@
+// App.js
+import './App.css';
+import Todo from './Todo';
+import React, {useState, useEffect} from "react";
+import {Container, List, Paper} from "@mui/material";
+import AddTodo from './AddTodo';
+import {call} from "./service/ApiService";
+function App(){
+ const [items, setItems] = useState([]);
+
+ useEffect(() => {
+ call("/todo", "GET", null)
+ .then((response) => setItems(response.data));
+
+ }, [])
+ const addItem = (item) => {
+ item.id = "ID-"+items.length;
+ item.done = false;
+ setItems([...items, item]);
+ console.log("items: ", items);
+ call("/todo","POST", item)
+ .then((response) => setItems(response.data)); //?결과확인해야함 response
+타입
+ };
+ let todoItems =
+ items.length > 0 && (
+ <Paper style={{ margine: 16 }} >
+ <List>
+ {items.map((item) => (
+ <Todo item={item} key={item.id} />
+ ))}
+ </List>
+ </Paper>
+ );
+return <div className = "App">
+ <Container maxWidth="md">
+ <AddTodo addItem={addItem}/>
+ {todoItems}
+ </Container>
+ </div>
+}
+export default App;
---
+++ frontend/src/Todo.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+import { ListItem, ListItemText, InputBase, Checkbox, 
+	ListItemSecondaryAction, IconButton } 
+	from "@mui/material";
+import DeleteOutlined from 
+               "@mui/icons-material/DeleteOutlined" ;
+
+const Todo = (props) => {
+	// eslint-disable-next-line no-unused-vars
+	const [item, setItem] = useState(props.item);
+
+	return(
+		<ListItem>
+			<Checkbox checked = {item.done} />
+      <ListItemText>
+				<InputBase
+					inputProps = {{ "aria-label": "naked" }}
+					type = "text"
+					id = {item.id}
+					name = {item.id}
+					value = {item.title}
+					multiline = {true}
+					fullWidth = {true}
+        />
+			</ListItemText>
+			<ListItemSecondaryAction>
+			<IconButton aria-label="Delete Todo" >
+          			<DeleteOutlined />
+				</IconButton>
+			</ListItemSecondaryAction>
+    </ListItem>
+
+ );
+};
+
+export default Todo;
